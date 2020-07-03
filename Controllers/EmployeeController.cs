6f55b71a034f071f6e6224d7eee3c6d55dfe34a5
Galina Aidemirska <galina.aidemirska@t4g.com>
2020-07-03 13:47:42 -0400

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreRestAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        /// <summary>
+        /// Gets the list of all Employees.
+        /// </summary>
+        /// <returns>The list of Employees.</returns>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return GetEmployees();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}", Name = "Get")]
+        public Employee Get(int id)
+        {
+            return GetEmployees().Find(e => e.Id == id);
+        }
+
+        /// <summary>
+        /// Creates an Employee.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST api/Employee
+        ///     {        
+        ///       "firstName": "Galina",
+        ///       "lastName": "Aidemirska",
+        ///       "emailId": "galia5gal@gmail.com"        
+        ///     }
+        /// </remarks>
+        /// <param name="employee"></param>  
+        /// <returns>A newly created employee</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>  
+        // POST: api/Employee
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [Produces("application/json")]
+        public Employee Post([FromBody] Employee employee)
+        {
+            // Logic to create new Employee
+            return new Employee();
+        }
+
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee employee)
+        {
+            // Logic to update an Employee
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+        private List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName= "John",
+                LastName = "Smith",
+                EmailId ="John.Smith@gmail.com"
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName= "Jane",
+                LastName = "Doe",
+                EmailId ="Jane.Doe@gmail.com"
+            }
+        };
+        }
+    }
+}
